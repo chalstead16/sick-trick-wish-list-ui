02adae340e333b6414110061d15c70d2a00660fa
Casey Halstead <catherineclosee@gmail.com>
2022-04-06 10:57:54 -0500

    Add methods to set tricks state with Tricks API data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,22 @@
 import { Component } from 'react';
 import './App.css';
+import getTricksData from '../../apiCalls';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      tricks: []
+    }
+  }
+
+  getTricks = () => {
+    getTricksData()
+    .then(data => this.setState({tricks: data}));
+  }
+
+  componentDidMount = () => this.getTricks()
+
   render() {
     return (
       <div className="App">
