1707cc762bcc6fc3906538a2cf20de16e0b0d50e
Casey Halstead <catherineclosee@gmail.com>
2022-04-06 11:52:13 -0500

    Add HTML elements to TrickCard component

---
+++ src/components/TrickCard/TrickCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TrickCard = ({stance, name, obstacle, tutorial}) => {
+    return (
+        <div className='card'>
+            <p>{stance} {name}</p>
+            <p>Obstacle: {obstacle}</p>
+            <p>Link to Tutorial:</p>
+            <p>{tutorial}</p>
+        </div>
+    )
+}
+
+export default TrickCard;
